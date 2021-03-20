b12cb38b5016621fe4ecd2f6a1177c8e86ece5fd
vaanya-the-coder <72737233+vaanya-the-coder@users.noreply.github.com>
2021-03-20 11:40:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var snake,snakeImg;
+var appleImg,bananaImg,grapesImg,orangeImg,strawberryImg;
+var backgroundImg;
+var w,h;
+var rez = 20;
+
+function preload(){
+    backgroundImg = loadImage("images/background.png");
+    appleImg = loadImage("images/apple.png");
+    bananaImg = loadImage("images/banana.png");
+    grapesImg = loadImage("images/grapes.png");
+    orangeImg = loadImage("images/orange.png");
+    strawberryImg = loadImage("images/strawberry.png");
+}
+
+function setup(){
+    createCanvas(400,400);
+    w = floor(width/rez);
+    h = floor(height/rez);
+    frameRate(5);
+
+}
+
+function draw(){
+    background(backgroundImg);
+}
